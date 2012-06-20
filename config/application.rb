ff4c8bf2ca28ddd1a7ad3d14d438ffb2b959efee
Tad Thorley <phaedryx@gmail.com>
2012-06-20 07:24:57 -0600

    change app config for heroku deployment, see: https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Templates
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # this needs to be set for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
